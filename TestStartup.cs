cafdc69ff1197c4abf7f697d48da899c4ea9c961
Batman <57849598+eat-sleep-code-repeat-it@users.noreply.github.com>
2020-09-23 19:21:19 -0400

    Add files via upload
    specflow.test

---
+++ TestStartup.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NetCoreWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCoreWebApi
+{
+    public class TestStartup
+    {
+        public TestStartup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+        // This method gets called by the runtime. Use this method to addservices to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            // services.AddDbContext<BooksContext>(opt => opt.UseSqlite("Data Source=books.db"));
+            services.AddScoped<IBookChaptersRepository, BookChaptersRepository>();
+            // *** Do not add custom services here
+        }
+        // This method gets called by the runtime. Use this method to configurethe HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            // *** Do not configure Authentication here
+        }
+    }
+}
